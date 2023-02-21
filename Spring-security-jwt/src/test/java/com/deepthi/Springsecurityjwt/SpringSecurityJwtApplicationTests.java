61ab85206b8f2faf551ef66fba507043e6570dc3
Deepthi <deepthireddyg@gmail.com>
2023-02-21 16:11:17 -0500

    first commit

---
+++ Spring-security-jwt/src/test/java/com/deepthi/Springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepthi.Springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
